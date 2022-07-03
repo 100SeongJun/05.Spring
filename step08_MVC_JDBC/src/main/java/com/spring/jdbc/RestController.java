7b8f33ffc51f2b5693ac2c0d5dcbae48f753c8a9
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-07-03 15:11:18 +0900

    feat : RestApi

--- step08_MVC_JDBC/src/main/java/com/spring/jdbc/RestController.java
+++ step08_MVC_JDBC/src/main/java/com/spring/jdbc/RestController.java
@@ -1,5 +1,6 @@
 package com.spring.jdbc;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,19 +8,17 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.dto.Dept;
 import com.spring.service.DeptServiceImpl;
 
 @org.springframework.web.bind.annotation.RestController
 // @Controller + @Responsebody
-
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class RestController {
 
 	@Autowired
@@ -49,45 +48,32 @@ public class RestController {
 	 * // @GetMapping(value = "/api/dept/{deptno}/{dname}") // public List<Dept>
 	 * deptData() { // return deptService.getAllDepts(); // }
 	 */
-	@PostMapping(value = "/api/deptjson", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void insertDeptForm3(@RequestBody Dept dept) {
-//		System.out.println(dept);
-		deptService.insertDept(dept);
-	}
-
-	@PostMapping(value = "api/deptjson", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public void insertDeptForm4(Dept dept) {
-//		System.out.println(dept);
-		deptService.insertDept(dept);
-	}
-
-	@GetMapping(value = "/api/no-proxy")
-	public String noProxy() {
-		System.out.println("/no-proxy");
-		return "no-proxy";
-	}
-
-	@GetMapping(value = "/api/proxy")
-	public String Proxy() {
-		System.out.println("/proxy");
-		return "proxy";
-	}
-
-	@GetMapping(value = "/api/no-cors")
-	public String noCors() {
-		System.out.println("/no-cors");
-		return "no-cors";
-	}
-
-	@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4000" })
-	@GetMapping(value = "/api/cors")
-	public String cors() {
-		System.out.println("/cors");
-		return "cors";
-	}
-
+	/*
+	 * @PostMapping(value = "/api/deptjson", consumes =
+	 * MediaType.APPLICATION_JSON_VALUE) public void insertDeptForm3(@RequestBody
+	 * Dept dept) { // System.out.println(dept); deptService.insertDept(dept); }
+	 * 
+	 * @PostMapping(value = "api/deptjson", consumes =
+	 * MediaType.APPLICATION_FORM_URLENCODED_VALUE) public void insertDeptForm4(Dept
+	 * dept) { // System.out.println(dept); deptService.insertDept(dept); }
+	 * 
+	 * @GetMapping(value = "/api/no-proxy") public String noProxy() {
+	 * System.out.println("/no-proxy"); return "no-proxy"; }
+	 * 
+	 * @GetMapping(value = "/api/proxy") public String Proxy() {
+	 * System.out.println("/proxy"); return "proxy"; }
+	 * 
+	 * @GetMapping(value = "/api/no-cors") public String noCors() {
+	 * System.out.println("/no-cors"); return "no-cors"; }
+	 * 
+	 * @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4000" })
+	 * 
+	 * @GetMapping(value = "/api/cors") public String cors() {
+	 * System.out.println("/cors"); return "cors"; }
+	 */
+//---------------------------------------------------------------------------------------------------------//
 	// 모두 출력
-	@GetMapping(value = "/api/dept", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@GetMapping(value = "/api/dept")
 	public List<Dept> getAllDepts() {
 		return deptService.getAllDepts();
 	}
@@ -99,26 +85,24 @@ public class RestController {
 	}
 
 	// 특정 삭제
-	@DeleteMapping(value = "/api/dept", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public List<Dept> deleteDept(@RequestParam("deptno") int deptno) {
-		System.out.println(deptno);
+	@DeleteMapping(value = "/api/dept/{deptno}")
+	public List<Dept> deleteDept(@PathVariable("deptno") int deptno) {
 		deptService.deleteDept(deptno);
 		return deptService.getAllDepts();
 	}
 
 	// 수정
-	@PutMapping(value = "/api/dept", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public List<Dept> updateDept(@RequestBody Dept dept) {
-		System.out.println(dept);
+	@PutMapping(value = "/api/dept/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public List<Dept> updateDept(@PathVariable("id") int id, @RequestBody HashMap<String, Object> map) {
+		Dept dept = new Dept(id, (String) map.get("dname"), (String) map.get("loc"));
 		deptService.updateDept(dept);
 		return deptService.getAllDepts();
 	}
 
 	// 생성
 	@PostMapping(value = "/api/dept")
-	public List<Dept> insertDept(@ModelAttribute Dept dept) {
-		System.out.println(dept);
+	public void insertDept(Dept dept) {
 		deptService.insertDept(dept);
-		return deptService.getAllDepts();
+		return;
 	}
 }
